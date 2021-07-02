524c7dee03680e247ec05238d0a7b392d3a956a2
Nathaniel <NathanielKerr10@gmail.com>
2021-07-01 18:34:11 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function()  {
+  addTask();
+})
+
+function addTask() {
+  const form  = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+    const elList = document.getElementById("tasks");
+    const elItem = document.createElement("li");
+    elItem.innerText = task;
+    elList.appendChild(elItem);
+  })  
+}
